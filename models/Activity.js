c4d85b98c9e096312761e0af10c6e5014573cd63
Muhammad Ilyas <53975283+milyas26@users.noreply.github.com>
2020-10-02 19:08:50 +0700

    CRUD Activity Done

--- models/Activity.js
+++ models/Activity.js
@@ -10,7 +10,7 @@ const activitySchema = new mongoose.Schema({
         type : String,
         required : true
     },
-    imageURL : {
+    imageUrl : {
         type : String,
         required : true
     },
