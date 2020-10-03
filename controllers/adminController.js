d741fe23dfe0f38df9a56d91f0d14fee60bb33cc
Muhammad Ilyas <53975283+milyas26@users.noreply.github.com>
2020-10-03 21:03:39 +0700

    Dashboard Project Staycation Done

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -63,17 +63,22 @@ module.exports = {
     req.session.destroy();
     res.redirect('/admin/signin');
   },
-
+  
   //DASHBOARD
-  viewDashboard: (req, res) => {
+  viewDashboard: async (req, res) => {
     try {
-      
+      const member = await Member.find();
+      const booking = await Booking.find();
+      const item = await Item.find();
       res.render('admin/dashboard/view_dashboard', {
         title : "Staycation | Dashboard",
-        user : req.session.user
+        user : req.session.user,
+        member,
+        booking,
+        item
       })
     } catch (error) {
-      
+      res.redirect('/admin/dashboard');
     }
   },
 
